f97fc87740b86c608455563968f54f85090f3360
DraconDev <dracsharp@gmail.com>
2024-12-21 07:21:03 +0000

    upd wxt config

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'wxt';
-
+import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
+    extensionApi: "chrome",
+    manifest: {
+        name: "",
+        description: "",
+        version: "1.1.0",
+        permissions: ["storage"],
+        icons: {
+            "16": "icon/16.png",
+            "32": "icon/32.png",
+            "48": "icon/48.png",
+            "96": "icon/96.png",
+            "128": "icon/128.png",
+        },
+        action: {
+            default_popup: "popup.html",
+            default_icon: {
+                "16": "icon/16.png",
+                "32": "icon/32.png",
+                "48": "icon/48.png",
+                "128": "icon/128.png",
+            },
+            default_title: "",
+        },
+    },
+    modules: ["@wxt-dev/module-react"],
 });
