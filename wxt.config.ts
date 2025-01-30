054f58b20bc7503c3ec30b5df187338282c4d702
DraconDev <dracsharp@gmail.com>
2025-01-30 00:07:50 +0000

    feat(config): Update extension manifest details

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,29 +1,31 @@
 import { defineConfig } from "wxt";
-// See https://wxt.dev/api/config.html
+
 export default defineConfig({
-    extensionApi: "chrome",
-    manifest: {
-        name: "Example Extension",
-        description: "Example Extension",
-        version: "0.0.1",
-        permissions: ["storage"],
-        icons: {
-            "16": "icon/16.png",
-            "32": "icon/32.png",
-            "48": "icon/48.png",
-            "96": "icon/96.png",
-            "128": "icon/128.png",
-        },
-        action: {
-            default_popup: "popup.html",
-            default_icon: {
-                "16": "icon/16.png",
-                "32": "icon/32.png",
-                "48": "icon/48.png",
-                "128": "icon/128.png",
-            },
-            default_title: "",
-        },
+  extensionApi: "chrome",
+  manifest: {
+    name: "Auto Fullscreen",
+    description:
+      "Automatically enters fullscreen on hover and exits when mouse moves to top of screen",
+    version: "0.0.2",
+    permissions: ["storage", "fullscreen"],
+    host_permissions: ["<all_urls>"],
+    icons: {
+      "16": "icon/16.png",
+      "32": "icon/32.png",
+      "48": "icon/48.png",
+      "96": "icon/96.png",
+      "128": "icon/128.png",
     },
-    modules: ["@wxt-dev/module-react"],
+    action: {
+      default_popup: "popup.html",
+      default_icon: {
+        "16": "icon/16.png",
+        "32": "icon/32.png",
+        "48": "icon/48.png",
+        "128": "icon/128.png",
+      },
+      default_title: "Auto Fullscreen",
+    },
+  },
+  modules: ["@wxt-dev/module-react"],
 });
