6f68c165503ba1abb58c4d65feda613866d50d16
DraconDev <dracsharp@gmail.com>
2025-01-30 00:26:55 +0000

    refactor(background): Remove background script

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,12 +1,1 @@
-import { store } from "@/utils/store";
-
-// Listen for keyboard command
-chrome.commands.onCommand.addListener(async (command) => {
-  if (command === "toggle_enabled") {
-    const currentState = await store.getValue();
-    await store.setValue({
-      ...currentState,
-      enabled: !currentState.enabled,
-    });
-  }
-});
+// Background script no longer needed - functionality handled by content script and popup
