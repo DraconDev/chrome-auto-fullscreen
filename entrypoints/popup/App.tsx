811eca4c12461a1087bcd8ddd2453eb8ecf0217e
DraconDev <dracsharp@gmail.com>
2025-03-20 22:25:18 +0000

    docs(popup): Clarify fullscreen activation behavior

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -144,10 +144,8 @@ function App() {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              Clicks only switch to fullscreen mode
+              First click on page only activates fullscreen
             </p>
-
-
           </div>
         </div>
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     name: "Auto Fullscreen",
     description:
       "Click anywhere to enter fullscreen mode, move cursor to top edge to exit. Simple and intuitive.",
-    version: "1.1.19",
+    version: "1.1.20",
     permissions: ["storage"],
     host_permissions: ["<all_urls>"],
     icons: {
