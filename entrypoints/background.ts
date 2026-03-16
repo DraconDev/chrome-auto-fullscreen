c03a7243e2c8e76466b18ad78a9845e370a911da
DraconDev <dracsharp@gmail.com>
2026-03-16 16:44:27 +0000

    chore(misc): wip checkpoint --- category: chore scope: misc ---

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -34,27 +34,20 @@ export default defineBackground({
       }
 
       if (message.action === "setWindowFullscreen") {
-        console.log("[AF BG] setWindowFullscreen received");
         chrome.windows.getCurrent((win) => {
           if (win.id === undefined) return;
           if (win.state !== "fullscreen") {
-            chrome.windows.update(win.id, { state: "fullscreen" }, () => {
-              console.log("[AF BG] entered fullscreen");
-            });
+            chrome.windows.update(win.id, { state: "fullscreen" });
           }
         });
         return false;
       }
 
       if (message.action === "exitWindowFullscreen") {
-        console.log("[AF BG] exitWindowFullscreen received");
         chrome.windows.getCurrent((win) => {
-          console.log("[AF BG] current window state:", win.state);
           if (win.id === undefined) return;
           if (win.state === "fullscreen") {
-            chrome.windows.update(win.id, { state: "normal" }, () => {
-              console.log("[AF BG] exited fullscreen");
-            });
+            chrome.windows.update(win.id, { state: "normal" });
           }
         });
         return false;
